c8dbab3b88dd59719e66967b6dde332db35fd470
Andrew Kobelev <heiskobe@yandex.ru>
2024-12-25 00:48:22 +0300

    Upgrade readme and title with description for api doc

--- app/core/config.py
+++ app/core/config.py
@@ -4,8 +4,14 @@ from pydantic import BaseSettings, EmailStr
 
 
 class Settings(BaseSettings):
-    app_title: str = 'Title'
-    description: str = 'Description'
+    app_title: str = 'Благотворительный фонд'
+    description: str = (
+        'Фонд собирает пожертвования на различные целевые проекты: '
+        'на медицинское обслуживание нуждающихся хвостатых, '
+        'на обустройство кошачьей колонии в подвале, на '
+        'корм оставшимся без попечения кошкам — на любые цели, '
+        'связанные с поддержкой кошачьей популяции'
+    )
     database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     secret: str = 'SECRET'
     first_superuser_email: Optional[EmailStr] = None
